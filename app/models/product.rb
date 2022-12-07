ee4f14d80e48e3aecaa36d318b30cbaac9fd2bba
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-12-07 15:30:08 -0500

    security push

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,10 @@ class Product < ApplicationRecord
     has_many :carts, through: :cart_items
     has_many :orders
 
-    # validates_presence_of :title, :roast, :description, :price, :tasting_notes, :image_url
-    # validates :title, uniqueness: true
-    # validates :single_origin, inclusion: { in: %w(true false), message: "%{value} is not a valid boolean" }
-    # validates :image_url, uniqueness: true
-    # validates :price, numericality: { greater_than: 0 }
-    # validates :description, length: { in: 65..800 }
-    # validates :roast, inclusion: { in: %w(Light Medium Dark Blend), message: "input has angered the coffee gods! '%{value}' is not a valid roast!" }
+    validates_presence_of :title, :roast, :description, :price, :tasting_notes, :image_url
+    validates :title, uniqueness: true
+    validates :image_url, uniqueness: true
+    validates :price, numericality: { greater_than: 0 }
+    validates :description, length: { in: 65..800 }
+    validates :roast, inclusion: { in: %w(Light Medium Dark Blend), message: "input has angered the coffee gods! '%{value}' is not a valid roast!" }
 end
