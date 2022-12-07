199480aa7bc152ae1536f9ca56dd86f9732b47cc
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-12-07 13:58:59 -0500

    security push

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,13 +5,11 @@ class Product < ApplicationRecord
     has_many :carts, through: :cart_items
     has_many :orders
 
-    validates_presence_of :title, :roast, :description, :price, :tasting_notes, :image_url
-    #will need to put additional info back, when it exists; along with featured, single_origin, and inStock (all booleans that would not be accepted unless set to true)
-    validates :title, uniqueness: true
-    validates :single_origin, inclusion: { in: %w(true false), message: "%{value} is not a valid boolean" }
-    # I don't know why this doesn't work, but I'm not sure it's necessary anyway
+    # validates_presence_of :title, :roast, :description, :price, :tasting_notes, :image_url
+    # validates :title, uniqueness: true
+    # validates :single_origin, inclusion: { in: %w(true false), message: "%{value} is not a valid boolean" }
     # validates :image_url, uniqueness: true
-    validates :price, numericality: { greater_than: 0 }
-    validates :description, length: { in: 65..800 }
-    validates :roast, inclusion: { in: %w(Light Medium Dark Blend), message: "input has angered the coffee gods! '%{value}' is not a valid roast!" }
+    # validates :price, numericality: { greater_than: 0 }
+    # validates :description, length: { in: 65..800 }
+    # validates :roast, inclusion: { in: %w(Light Medium Dark Blend), message: "input has angered the coffee gods! '%{value}' is not a valid roast!" }
 end
